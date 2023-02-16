8da292b0a45868e6abfe77a2c92efe9e6e7f7898
OriginalByteMe <noahrijkaard@gmail.com>
2023-02-16 15:44:36 +0800

    Add amplify link for valid repo's + fix draft repo's still showing up

--- bot.js
+++ bot.js
@@ -36,14 +36,14 @@ if (
 
 // ? This is an object to assign colours to each embed depending on what repo it's from
 const repoList = {
-	[process.env.ANNOTATOR_REPO] : { colour: '#FF0000', PRs: [] },
-	[process.env.BOLT_REPO] : { colour: '#0000FF', PRs: [] },
-	[process.env.DATA_PIPE_REPO] : { colour: '#00FF00', PRs: [] },
-	[process.env.PROJECTS_REPO] : { colour: '#33FFF9', PRs: [] },
-	[process.env.PAYMENT_REPO] : { colour: '#FFA833', PRs: [] },
-	[process.env.KAYA_REPO] : { colour: '#DA33FF', PRs: [] },
-	[process.env.ADMIN_REPO] : { colour: '#9333FF', PRs: [] },
-	[process.env.PLAYGROUND_REPO] : { colour: '#E0FF33', PRs: [] },
+	[process.env.BOLT_REPO] : { colour: '#0000FF', PRs: [], amplify: true, pr_preview: 'https://test-pr-previews.dl5eewox9qmvx.amplifyapp.com/' },
+	[process.env.KAYA_REPO] : { colour: '#DA33FF', PRs: [], amplify: true, pr_preview: 'https://test-pr-previews.dk2lbhfysm2ic.amplifyapp.com/' },
+	[process.env.ADMIN_REPO] : { colour: '#9333FF', PRs: [], amplify: true, pr_preview: 'https://test.d1itkb6pz56lpp.amplifyapp.com/' },
+	[process.env.ANNOTATOR_REPO] : { colour: '#FF0000', PRs: [], amplify: false },
+	[process.env.DATA_PIPE_REPO] : { colour: '#00FF00', PRs: [], amplify: false },
+	[process.env.PROJECTS_REPO] : { colour: '#33FFF9', PRs: [], amplify: false },
+	[process.env.PAYMENT_REPO] : { colour: '#FFA833', PRs: [], amplify: false },
+	[process.env.PLAYGROUND_REPO] : { colour: '#E0FF33', PRs: [], amplify: false },
 };
 
 // Discord bot must collect all messages from channel, presume all messages are embeds
@@ -74,6 +74,25 @@ const checkIfPresent = async (channel, prURL) => {
 	return false;
 };
 
+const getAmplify = async (repo, prNum, prRewviewURL) => {
+	let amplifyLink = '\`PR Link:\` ';
+	const regex = /https:\/\/pr\-[^\s]+/;
+	const responses = await octokit.request(`GET /repos/${repo}/issues/${prNum}/comments`, {
+		owner: 'Supahands',
+		repo: repo.slice('/'),
+		pull_number: prNum,
+	});
+	for (const response of responses.data) {
+		if (response.user.login === 'aws-amplify-us-west-2[bot]') {
+			console.log('PR LINK:', response.body.match(regex)[0]);
+			amplifyLink += await response.body.match(regex)[0];
+			amplifyLink += `\n\n \`Test link to login first with:\` ${prRewviewURL}/`;
+			amplifyLink += '\n\n@Zi Wei @geethekthek';
+		}
+	}
+
+	return amplifyLink;
+};
 // TODO: Modify this, to accept the repo name as well, to assign colours to it
 const createEmbed = async (repo, pr, channel) => {
 
@@ -81,20 +100,46 @@ const createEmbed = async (repo, pr, channel) => {
 		return;
 	}
 
-	const Embed = new EmbedBuilder()
-		.setColor(repoList[repo].colour)
-		.setTitle(`${pr.number}: ${pr.title}`)
-		.setURL(pr.html_url)
-		.setAuthor({ name: pr.user.login, iconURL: null, url: pr.user.html_url })
-		.setThumbnail(pr.user.avatar_url)
-		.addFields({
-			name: 'Repo',
-			value: `${repo}`,
-		})
-		.setDescription(
-			`Merge ${pr.commits} commits into \`${pr.base.ref}\` from \`${pr.head.ref}\` @everyone`,
-		);
-	channel.send({ embeds: [Embed] });
+	const { number, title, commits } = pr;
+	const { avatar_url, login } = pr.user;
+	if (repoList[repo].amplify) {
+		const amplifyFieldData = await getAmplify(repo, number, repoList[repo].pr_preview);
+		const Embed = new EmbedBuilder()
+			.setColor(repoList[repo].colour)
+			.setTitle(`${number}: ${title}`)
+			.setURL(pr.html_url)
+			.setAuthor({ name: login, iconURL: null, url: pr.user.html_url })
+			.setThumbnail(avatar_url)
+			.addFields({
+				name: 'Repo',
+				value: `${repo}`,
+			},
+			{
+				name: 'Amplify details',
+				value: `${await amplifyFieldData}`,
+			})
+			.setDescription(
+				`Merge ${commits} commits into \`${pr.base.ref}\` from \`${pr.head.ref}\` @everyone`,
+			);
+		channel.send({ embeds: [Embed] });
+	}
+	else {
+
+		const Embed = new EmbedBuilder()
+			.setColor(repoList[repo].colour)
+			.setTitle(`${number}: ${title}`)
+			.setURL(pr.html_url)
+			.setAuthor({ name: login, iconURL: null, url: pr.user.html_url })
+			.setThumbnail(avatar_url)
+			.addFields({
+				name: 'Repo',
+				value: `${repo}`,
+			})
+			.setDescription(
+				`Merge ${commits} commits into \`${pr.base.ref}\` from \`${pr.head.ref}\` @everyone`,
+			);
+		channel.send({ embeds: [Embed] });
+	}
 };
 
 // TODO: Search through all messages, compare embed url with arguement URL, then delete message if it's the same
@@ -239,7 +284,7 @@ const updateChannel = async (channel) => {
 		// ? - If the PR is open and does not match any of the channel emebds url, create new embed and add to channel
 		for (const pr of prs) {
 			const prInChat = chatEmbeds.find(embed => embed.data.url === pr.html_url);
-			if (prInChat === undefined) {
+			if (prInChat === undefined && pr.draft === false) {
 				await createEmbed(repoItem, pr, channel);
 			}
 		}
